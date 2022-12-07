771e3848e7e632c20f343563afc884aa32322d32
dubiZA <darius@dubiza.io>
2022-12-06 21:57:54 -0700

    Add ability to adjust canvas resolution
    To enable the functionality to adjust resolution, I've added a
    button to the UI which prompts for a number between 16 and 100. The
    code checks the value is valid and alerts if it isn't. If it is,
    the canvas is cleared (all "pixels" are removed from DOM) and the
    canvas is redrawn with new "pixels". Several helper functions have
    been added to assist (calculating the "pixel" dimensions, etc) with
    all these requirements.

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
+const canvasDimension = 720;
+
 const containerDiv = document.querySelector(".container");
+containerDiv.addEventListener("mouseenter", () => turnPixelOn());
 
-containerDiv.addEventListener("mouseenter", () => getPixels());
+const changeResolution = document.querySelector(".changeResolution");
+changeResolution.addEventListener("click", () => {
+  let gridSize = parseInt(prompt("Enter a grid size from 16 to 100."));
+  if (gridSize >= 16 && gridSize <= 100) {
+    clearCanvas();
+    drawCanvas(gridSize);
+  } else {
+    alert("Invalid number, make sure it's between 16 and 100.");
+  }
+});
 
-function drawCanvas() {
+function drawCanvas(gridSize) {
+  let pixelDimension = calculateDimensions(gridSize);
 
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < gridSize; i++) {
     const pixelRowDiv = document.createElement("div");
     pixelRowDiv.classList.add("pixelRow");
+    pixelRowDiv.style.height = `${pixelDimension}px`;
     containerDiv.appendChild(pixelRowDiv);
   }
 
   const pixelRows = document.querySelectorAll(".pixelRow");
   pixelRows.forEach((row) => {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < gridSize; i++) {
       const pixel = document.createElement("div");
       pixel.classList.add("pixel");
+      pixel.style.width = `${pixelDimension}px`;
+      pixel.style.height =  `${pixelDimension}px`;
       row.appendChild(pixel);
     }
   });
 }
 
-function getPixels() {
+function turnPixelOn() {
   const pixels = document.querySelectorAll(".pixel");
   pixels.forEach(pixel => pixel.addEventListener("mouseenter", function() {
-    turnPixelOn(pixel);
+    pixel.classList.add("pixelOn");
   }));
 }
 
-function turnPixelOn(pixel) {
-  pixel.classList.add("pixelOn");
+function clearCanvas() {
+  const pixelRows = document.querySelectorAll(".pixelRow");
+  pixelRows.forEach((pixelRow) => {
+    pixelRow.remove();
+  });
+}
+
+function calculateDimensions(gridSize) {
+  return canvasDimension / gridSize;
 }
